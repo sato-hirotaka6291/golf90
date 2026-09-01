656fe5bd2c8b7168e3a84fc0d967bbfd70f0ad8f
sato-hirotaka6291 <157279825+sato-hirotaka6291@users.noreply.github.com>
2026-09-01 20:20:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='golf90-pwa-v1';
+const CACHE='golf90-pwa-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
